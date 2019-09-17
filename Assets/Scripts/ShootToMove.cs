5ab89ae4f04f5a84a594eaeaa955698f0126c29b
mjneff2 <mjneff2@illinois.edu>
2019-09-16 20:18:12 -0500

    feat: basic movement

---
+++ Assets/Scripts/ShootToMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootToMove : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Camera camera1;
+    public float timeBetweenShots = 2.0f;
+    public float lastShot = 0;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        camera1 = GetComponentInChildren<Camera>();
+    }
+
+    public void FixedUpdate() {
+        var forward = camera1.transform.forward;
+        forward.Normalize();
+        if (Input.GetButton("Fire1") && Time.time > lastShot + timeBetweenShots) {
+            rb.AddForce(forward * -300.0f);
+            lastShot = Time.time;
+            shoot();
+        }
+    }
+
+    void shoot() {
+        //ToDo
+    }
+}
