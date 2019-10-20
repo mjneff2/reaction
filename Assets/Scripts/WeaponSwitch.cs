3ebc530305623d7cbfc26e115bfc6a7a96842caa
mjneff2 <mjneff2@illinois.edu>
2019-10-20 14:44:06 -0500

    Added weapon switching with scroll wheel, weapons not unique yet

--- Assets/Scripts/WeaponSwitch.cs
+++ Assets/Scripts/WeaponSwitch.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class WeaponSwitch : MonoBehaviour
 {
+
+    public int currentWeapon = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        selectWeapon();
     }
 
     // Update is called once per frame
     void Update()
     {
+        int previousWeapon = currentWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+            if (currentWeapon >= transform.childCount - 1) {
+                currentWeapon = 0;
+            } else {
+                currentWeapon++;
+            }
+        } else if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
+            if (currentWeapon >= transform.childCount - 1) {
+                currentWeapon = 0;
+            } else {
+                currentWeapon++;
+            }
+        }
         
+        if (previousWeapon != currentWeapon) {
+            selectWeapon();
+        }
+    }
+
+    void selectWeapon() {
+        int k = 0;
+        foreach (Transform weapon in transform) {
+            if (k == currentWeapon) {
+                weapon.gameObject.SetActive(true);
+            } else {
+                weapon.gameObject.SetActive(false);
+            }
+            k++;
+        }
     }
 }
