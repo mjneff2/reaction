06afef5b25f66db4999f886e7909e30de011e035
mjneff2 <mjneff2@illinois.edu>
2019-10-23 17:16:51 -0500

    Added 2-player splitscreen

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    public string xAxis = "Mouse X";
+    public string yAxis = "Mouse Y";
+
     Vector2 rotation = new Vector2(0,0);
     public float sensitivity = 3;
     public float verticalMin = -30f;
@@ -17,8 +20,8 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rotation.y += Input.GetAxis("Mouse X");
-        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.y += Input.GetAxis(xAxis);
+        rotation.x += -Input.GetAxis(yAxis);
         rotation.x = Mathf.Clamp(rotation.x, verticalMin, verticalMax);
         transform.eulerAngles = (Vector2)rotation * sensitivity;
     }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth = 0;
+    private Vector3 originalPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        originalPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth < 1) {
+            gameObject.transform.position = originalPos;
+            currentHealth = maxHealth;
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "Bullet") {
+            currentHealth -= 20;
+            Destroy(col.gameObject);
+        }
+    }
+}
